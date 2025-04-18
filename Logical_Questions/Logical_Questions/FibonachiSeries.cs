185ed76033678728052dd70168872afc11babce9
shahejafar <shahejafarshaikh@gmail.com>
2025-04-18 16:16:19 +0530

    second question

---
+++ Logical_Questions/Logical_Questions/FibonachiSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Questions
+{
+    internal class FibonachiSeries
+    {
+
+        public void FiboSeries(int num) 
+        {
+
+            int num1 = 0;
+            int num2 = 1;
+            int num3;
+            Console.Write(num1 +" " +num2 );
+
+            for(int i =2; i < num; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(" "+num3);
+                num1 = num2;
+                num2 = num3;
+            }
+            
+        }
+    }
+}
