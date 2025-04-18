0fa743a09f308ba9fc78224ede84c4118265ec98
shahejafar <shahejafarshaikh@gmail.com>
2025-04-18 16:04:18 +0530

    Fisrt Question

---
+++ Logical_Questions/Logical_Questions/SwapNumberWithoutUsingThirdVar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Logical_Questions
+{
+    public class SwapNumberWithoutUsingThirdVar
+    {
+        public void SwapNumber(int a, int b)
+        {
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine(" The Value Of A is : "+a+ " \n The Value Of B is : "+b);
+            
+
+        }
+        
+
+
+        
+
+    }
+}
