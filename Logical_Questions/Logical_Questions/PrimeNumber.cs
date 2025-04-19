7a88c8e9d1d0a934634b42c0e65bdb1dc3ef4e40
shahejafar <shahejafarshaikh@gmail.com>
2025-04-19 16:28:32 +0530

    Third question

---
+++ Logical_Questions/Logical_Questions/PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Questions
+{
+    public class PrimeNumber
+    {
+        public void PrimeNum(int num)
+        {
+            bool flag = false;
+
+            for(int i= 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = true;
+                    break;
+                    
+                }
+
+            }
+            if(flag== false)
+            {
+                Console.WriteLine("Number {0} Is Prime Number. ", num);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} Is Not Prime Number. ", num);
+            }
+        }
+
+    }
+}
