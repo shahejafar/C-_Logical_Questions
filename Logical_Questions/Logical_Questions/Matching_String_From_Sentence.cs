83301c0f589ef26fa0154542e62e573e8c1d36f9
shahejafar <shahejafarshaikh@gmail.com>
2025-05-13 16:49:52 +0530

    string question

---
+++ Logical_Questions/Logical_Questions/Matching_String_From_Sentence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Questions
+{
+    internal class Matching_String_From_Sentence
+    {
+
+        public void MatchingString()
+        {
+            string str = "Shahejafar Shaikh Is Member Of Shaikh Family";
+
+            string[] arr=str.Split(' ');
+
+           for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=i+1; j<arr.Length; j++)
+                {
+                    if (arr[j] == arr[i])
+                    {
+                        Console.Write(arr[i]+" ");
+                    }
+                }
+            }
+        }
+    }
+}
