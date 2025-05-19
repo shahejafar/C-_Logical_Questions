10c3159463ceaccdfcf5cd580d956d6c068b5314
shahejafar <shahejafarshaikh@gmail.com>
2025-05-19 14:49:17 +0530

    Total word count

---
+++ Logical_Questions/Logical_Questions/Total_Words_FromString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Questions
+{
+    internal class Total_Words_FromString
+    {
+
+        public void Total_Words()
+        {
+            string str = "Shahejafar kamal shaikh";
+            string[] word= str.Split(' ');
+            int count = 0;
+
+            foreach (string word2 in word)
+            {
+                count++;
+            }
+            Console.WriteLine("Total Words : "+count);
+        }
+    }
+}
